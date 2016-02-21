c7f78df733d00c9bb77356238fa0da18dd6e25b3
Mariano Strugo <mariano.strugo@avatarla.com>
2016-02-21 19:43:35 -0300

    hash

--- builder/js/3_scripter.js
+++ builder/js/3_scripter.js
@@ -1,4 +1,9 @@
+if( window.location.hash == "" ){
+    window.location.hash = "#tab2";
+}
+
 function qS(selector) { return document.querySelector(selector); };
 
 // Create a new carousel.
 var carousel = new ch.Carousel(qS('.ch-carousel'), [{'async': 6, 'limitPerPage': 3 }]);
+
